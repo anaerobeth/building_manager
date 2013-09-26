abaf8fadee8983cdb06c386f5018d5344458d917
Beth Tenorio <betsky98@gmail.com>
2013-09-26 12:15:35 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BuildingManager::Application.config.secret_key_base = 'd5d4baa505827206a456bf009db92088e0a23e5bb0631f4576973529862e1720cd487dc8b0a2c89b41b03727fb94ddac78e3e289374178da5abc1a00be2d20f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BuildingManager::Application.config.session_store :cookie_store, key: '_building_manager_session'
