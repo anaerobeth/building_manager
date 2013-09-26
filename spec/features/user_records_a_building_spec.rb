e3a229d4cea3e12844d266cbf67d7a810aa4ca43
Beth Tenorio <betsky98@gmail.com>
2013-09-26 12:26:59 -0400

    Add spec to record a building

---
+++ spec/features/user_records_a_building_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+feature 'Record a building', %Q{
+  As a real estate associate
+ I want to record a building
+ So that I can refer back to pertinent information
+} do
+
+  #   Acceptance Criteria:
+
+  # I must specify a street address, city, state, and postal code
+  # Only US states can be specified
+  # I can optionally specify a description of the building
+  # If I enter all of the required information in the required format, the building is recorded.
+  # If I do not specify all of the required information in the required formats, the building is not recorded and I am presented with errors
+  # Upon successfully creating a building, I am redirected so that I can record another building.
+
+
+  scenario 'enters information in the required format' do
+    prev_count = Building.count
+    visit new_building_url
+    fill_in 'Street address', with: '15 Kneeland'
+    fill_in 'City', with: 'Boston'
+    fill_in 'State', with: 'MA'
+    fill_in 'Description', with: 'Luxury Loft'
+
+    click_button 'Record'
+    expect(page).to have_content('Building recorded')
+    expect(Building.count).to eql(prev_count + 1)
+  end
+
+  scenario 'does not enter information in the required format' do
+  end
+
+end
